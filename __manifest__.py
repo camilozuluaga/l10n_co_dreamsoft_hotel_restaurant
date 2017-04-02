f6790ab648fe290ff67ef69fd14ff1f92a2407af
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2017-04-02 16:19:34 -0500

    agregando estructura basica para iniciar modulo

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Colombia - Restaurante',
+    'category': 'Localization',
+    'version': '1.0.0',
+    'author': 'DREAMSOFT',
+    'license': 'AGPL-3',
+    'maintainer': '',
+    'website': '',
+    'summary': 'Colombia Restaurante: Extended hotel_restaurant'
+               'Contact Module - Odoo 10.0',
+    'images': [],
+    'depends': ['hotel_restaurant'],
+    'data': [
+
+    ],
+    'qweb': [],
+    'installable': True,
+}
