69677294212bc1489633f8c19ab582a6e3d93701
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2017-04-08 09:40:54 -0500

    agregando dependencia al modulo de hotel nuestro

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'summary': 'Colombia Restaurante: Extended hotel_restaurant'
                'Contact Module - Odoo 10.0',
     'images': [],
-    'depends': ['hotel_restaurant'],
+    'depends': ['l10n_co_dreamsoft_hotel','hotel_restaurant'],
     'data': [
         'views/hotel_reservation_order_inherit_view.xml',
         'views/hotel_restaurant_reservation.xml',
